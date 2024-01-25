25e480d631a9d017af8815e36108a1a0a006128f
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-25 13:45:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { itemsBoughtArr } from '/itemsBoughtArr.js'
+
+function calculateTotalCost(itemsBoughtArr, discount){
+
+    const total = itemsBoughtArr.reduce((total, currentItem)=>
+        total + currentItem.priceUSD, 0
+        )
+    return total - discount
+}
+
+console.log(calculateTotalCost(itemsBoughtArr))
